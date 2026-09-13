82850c20e00244d3316f44d39635a2084a652690
Nurbol Zhexenbiev <viltia40@gmail.com>
2026-09-13 14:38:48 +0500

    Update countries.ts

--- src/api/countries.ts
+++ src/api/countries.ts
@@ -44,7 +44,7 @@ export const getCountries = async (): Promise<Country[]> => {
       "https://api.restcountries.com/countries/v5",
       {
         params: {
-          limit: 120,
+          limit: 100,
         },
         headers: {
           Authorization: `Bearer ${apiKey}`,
